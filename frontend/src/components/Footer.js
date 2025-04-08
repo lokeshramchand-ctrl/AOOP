e8dc47cee26c79cfd23589fe3171ad2f8a73e21a
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-04-08 13:23:46 +0530

    REFRESH

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -56,28 +56,6 @@ const Footer = () => {
             </Box>
           </Grid>
 
-          {/* Contact Information Section */}
-          <Grid item xs={12} md={4}>
-            <Typography variant="h6" sx={{ fontWeight: 600, marginBottom: '1rem' }}>
-              Contact Us
-            </Typography>
-            <Typography variant="body2">
-              Project Maintainer:{' '}
-              <Link href="https://github.com/hoangsonww" color="inherit" sx={{ textDecoration: 'underline', '&:hover': { color: '#f57c00' } }}>
-                Son Nguyen
-              </Link>
-            </Typography>
-            <Typography variant="body2">
-              Email:{' '}
-              <Link href="mailto:hoangson091104@gmail.com" color="inherit" sx={{ textDecoration: 'underline', '&:hover': { color: '#f57c00' } }}>
-                hoangson091104@gmail.com
-              </Link>
-            </Typography>
-            <Typography variant="body2">Phone: +1 (123) 456-7890</Typography>
-            <Typography variant="body2" sx={{ marginTop: '0.5rem' }}>
-              Address: 123 Employee St, Suite 100, New York, NY 10001
-            </Typography>
-          </Grid>
         </Grid>
 
         {/* Bottom Section */}
